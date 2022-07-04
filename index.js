ddd25c61ccb51ec525ae8a68c36372ab257ae092
Yevhen <Mokaron4eg@gmail.com>
2022-07-04 22:32:35 +0300

    add dockerfile, cors and route

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors')
+const db = require('./db');
+
+const storage = db.productsStorage;
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/storage', (req, res) => {
+  res.json({ storage: storage });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
